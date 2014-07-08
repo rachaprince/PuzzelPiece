2210b9c7b740ab7b9bcb37b007499ef02b8e615e
Ben Burke <benjamin.burke@yale.edu>
2014-07-08 10:30:02 -0400

    semi-functioning, working to allow members to leave teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,5 @@
 class Team < ActiveRecord::Base
+	has_and_belongs_to_many :users
 	has_many :memberships, dependent: :destroy
 	has_many :users, through: :memberships
 	has_one :messageboard, dependent: :destroy
